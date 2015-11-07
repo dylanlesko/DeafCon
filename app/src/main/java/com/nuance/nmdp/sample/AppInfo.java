c16394f87302fef2cb3dc62e4827380d9df6cd3c
dylan <dylan@lesko.io>
2015-11-07 11:59:20 -0500

    YOLO

---
+++ app/src/main/java/com/nuance/nmdp/sample/AppInfo.java
@@ -0,0 +1,41 @@
+package com.nuance.nmdp.sample;
+
+public class AppInfo
+{
+    /**
+     * The login parameters should be specified in the following manner:
+     * 
+     * public static final String SpeechKitServer = "ndev.server.name";
+     * 
+     * public static final int SpeechKitPort = 1000;
+     * 
+     * public static final String SpeechKitAppId = "ExampleSpeechKitSampleID";
+     * 
+     * public static final byte[] SpeechKitApplicationKey =
+     * {
+     *     (byte)0x38, (byte)0x32, (byte)0x0e, (byte)0x46, (byte)0x4e, (byte)0x46, (byte)0x12, (byte)0x5c, (byte)0x50, (byte)0x1d,
+     *     (byte)0x4a, (byte)0x39, (byte)0x4f, (byte)0x12, (byte)0x48, (byte)0x53, (byte)0x3e, (byte)0x5b, (byte)0x31, (byte)0x22,
+     *     (byte)0x5d, (byte)0x4b, (byte)0x22, (byte)0x09, (byte)0x13, (byte)0x46, (byte)0x61, (byte)0x19, (byte)0x1f, (byte)0x2d,
+     *     (byte)0x13, (byte)0x47, (byte)0x3d, (byte)0x58, (byte)0x30, (byte)0x29, (byte)0x56, (byte)0x04, (byte)0x20, (byte)0x33,
+     *     (byte)0x27, (byte)0x0f, (byte)0x57, (byte)0x45, (byte)0x61, (byte)0x5f, (byte)0x25, (byte)0x0d, (byte)0x48, (byte)0x21,
+     *     (byte)0x2a, (byte)0x62, (byte)0x46, (byte)0x64, (byte)0x54, (byte)0x4a, (byte)0x10, (byte)0x36, (byte)0x4f, (byte)0x64
+     * };
+     * 
+     * Please note that all the specified values are non-functional
+     * and are provided solely as an illustrative example.
+     * 
+     */
+
+    /* Please contact Nuance to receive the necessary connection and login parameters */
+    public static final String SpeechKitServer = "sandbox.nmdp.nuancemobility.net";
+
+    public static final int SpeechKitPort = 443;
+    
+    public static final boolean SpeechKitSsl = false;
+
+    public static final String SpeechKitAppId = "NMDPTRIAL_djl237_scarletmail_rutgers_edu20151107084945";
+
+    public static final byte[] SpeechKitApplicationKey = {
+            (byte)0xa5, (byte)0x2f, (byte)0x92, (byte)0x00, (byte)0xe8, (byte)0xbc, (byte)0xa4, (byte)0x12, (byte)0x70, (byte)0xf0, (byte)0x95, (byte)0x13, (byte)0xec, (byte)0xaa, (byte)0xfb, (byte)0x6e, (byte)0x69, (byte)0x5c, (byte)0x89, (byte)0xa2, (byte)0x84, (byte)0x14, (byte)0xf5, (byte)0xda, (byte)0x98, (byte)0xcc, (byte)0x5b, (byte)0x95, (byte)0xa5, (byte)0xea, (byte)0x6a, (byte)0x4a, (byte)0x19, (byte)0xfc, (byte)0x4a, (byte)0x01, (byte)0xb8, (byte)0xee, (byte)0x14, (byte)0x28, (byte)0xdf, (byte)0xa9, (byte)0x3b, (byte)0xcb, (byte)0x54, (byte)0x36, (byte)0x39, (byte)0xbd, (byte)0x93, (byte)0xc2, (byte)0x5f, (byte)0x17, (byte)0xd6, (byte)0x54, (byte)0xf1, (byte)0x6e, (byte)0x21, (byte)0x4d, (byte)0x03, (byte)0xc6, (byte)0x37, (byte)0x6e, (byte)0x10, (byte)0x50
+    };
+}
